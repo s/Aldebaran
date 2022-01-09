d1d430ab5059b32c425c362c09fb1f35584ee7fd
Said Ozcan <saidozcn@gmail.com>
2022-01-09 22:24:06 +0100

    add localization framework

---
+++ Modules/Core/Localization/Localization/Localization.h
@@ -0,0 +1,18 @@
+//
+//  Localization.h
+//  Localization
+//
+//  Created by Said Özcan on 09/01/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Localization.
+FOUNDATION_EXPORT double LocalizationVersionNumber;
+
+//! Project version string for Localization.
+FOUNDATION_EXPORT const unsigned char LocalizationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Localization/PublicHeader.h>
+
+
