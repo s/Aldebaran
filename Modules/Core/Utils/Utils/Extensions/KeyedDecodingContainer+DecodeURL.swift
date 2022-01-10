d56363ce914a46709264d7987c956ca26de40217
Said Ozcan <saidozcn@gmail.com>
2022-01-10 23:40:45 +0100

    add decodeURLIfPresent extension

--- Modules/Core/Utils/Utils/Extensions/KeyedDecodingContainer+DecodeURL.swift
+++ Modules/Core/Utils/Utils/Extensions/KeyedDecodingContainer+DecodeURL.swift
@@ -8,6 +8,13 @@
 import Foundation
 
 public extension KeyedDecodingContainer  {
+    func decodeURLIfPresent(keyedBy: Key) throws -> URL? {
+        guard let urlString = try decodeIfPresent(String.self, forKey: keyedBy) else {
+            return nil
+        }
+        return URL(string: urlString)
+    }
+
     func decodeURL(keyedBy: Key) throws -> URL? {
         let urlString = try decode(String.self, forKey: keyedBy)
         return URL(string: urlString)
